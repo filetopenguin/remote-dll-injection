c484b1bf1d54934cb94a3418437bd330b2d7b684
filetopenguin <arielweiser17@gmail.com>
2024-03-30 14:52:35 +0300

    setting up consts and functions

---
+++ main.py
@@ -0,0 +1,22 @@
+from ctypes import *
+from ctypes import wintypes
+
+#constants:
+LPCTSTR = c_char_p
+SIZE_T  = c_size_t
+kernell32 = windll.kernel32
+
+#function declerations
+OpenProcess = kernel32.OpenProcess
+OpenProcess.argtypes = (wintypes.DWORD, wintypes.BOOL, wintypes.DWORD)
+OpenProcess.restype = wintypes.HANDLE
+
+VirtualAllocEx = kernel32.VirtualAllocEx
+VirtualAllocEx.argtypes = (wintypes.HANDLE, wintypes.LPVOID, SIZE_T, wintypes.DWORD, wintypes.DWORD)
+VirtualAllocEx.retype = wintypes.LPVOID
+
+WriteProcessMemory = kernel32.WriteProcessMemory
+WriteProcessMemory.argtypes = (wintypes.HANDLE, wintypes.LPVOID, wintypes.LPCVOID, SIZE_T, POINTER(SIZE_T))
+WriteProcessMemory.restype =  wintypes.BOOL
+
+
